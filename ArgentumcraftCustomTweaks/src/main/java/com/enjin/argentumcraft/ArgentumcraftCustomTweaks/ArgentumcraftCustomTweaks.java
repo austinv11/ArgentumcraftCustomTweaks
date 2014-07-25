5bcf25e27218c52b2dff43d9188e592710869062
austinv11 <austinv11@gmail.com>
2014-07-25 14:39:34 -0400

    Starting the actual plugin

--- ArgentumcraftCustomTweaks/src/main/java/com/enjin/argentumcraft/ArgentumcraftCustomTweaks/ArgentumcraftCustomTweaks.java
+++ ArgentumcraftCustomTweaks/src/main/java/com/enjin/argentumcraft/ArgentumcraftCustomTweaks/ArgentumcraftCustomTweaks.java
@@ -1,5 +1,31 @@
 package com.enjin.argentumcraft.ArgentumcraftCustomTweaks;
 
-public class ArgentumcraftCustomTweaks {
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ArgentumcraftCustomTweaks extends JavaPlugin{
 	
+	FileConfiguration config = getConfig();
+	
+	@Override
+	public void onEnable(){
+	configInit();	
+	}
+	
+	@Override
+	public void onDisable(){}
+	
+	private static void configInit(){
+		
+	}
+	
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
+		if (cmd.getName().equalsIgnoreCase("opengui")){
+			return true;
+		}
+		return false;
+	}
 }
